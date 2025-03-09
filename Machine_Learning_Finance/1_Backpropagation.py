4a38724286f1a3c82d13673df8dfd90b9714519b
Santiago Cruz <161742373+santiago-cruzlopez@users.noreply.github.com>
2025-03-08 21:23:35 -0500

    Update 1_Backpropagation.py

--- Machine_Learning_Finance/1_Backpropagation.py
+++ Machine_Learning_Finance/1_Backpropagation.py
@@ -1,3 +1,13 @@
+'''
+Backpropagation is the neural network training process of feeding 
+error rates back through a neural network to make it more accurate.
+
+Sources: 
+https://www.geeksforgeeks.org/backpropagation-in-neural-network/
+https://iamtrask.github.io/2015/07/12/basic-python-network/
+http://neuralnetworksanddeeplearning.com/chap2.html
+'''
+
 import numpy as np
 
 # Sigmoid Function
@@ -18,7 +28,7 @@ y = np.array([[0,0,1,1]]).T
 # Random Numbers for Calculation
 np.random.seed(1)
 
-# Initialize Weights Randomly with Mean 0
+# Initialize Weights Randomly with Mean 0 || First layer of weights, Synapse 0, connecting l0 to l1.
 syn0 = 2*np.random.random((3,1)) - 1
 
 for iter in range(10000):

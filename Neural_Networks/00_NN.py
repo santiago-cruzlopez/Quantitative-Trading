2df7d6ca94063dcff0de3234dcea34205368d750
Santiago Cruz <byte.alpha.tech@gmail.com>
2025-07-12 22:49:16 -0400

    NN_Theory

---
+++ Neural_Networks/00_NN.py
@@ -0,0 +1,10 @@
+input = [1, 2, 3]
+weights = [0.2, 0.8, -0.5]
+bias = 2
+
+output= (input[0] * weights[0] +
+          input[1] * weights[1] +
+          input[2] * weights[2] +
+          bias)
+
+print(output)

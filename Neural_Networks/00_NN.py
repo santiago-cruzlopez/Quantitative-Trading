3625fca06ad6f3d4beed26f3c58adafc996958b9
Santiago Cruz <byte.alpha.tech@gmail.com>
2025-07-13 23:35:46 -0400

    NN_Theory

--- Neural_Networks/00_NN.py
+++ Neural_Networks/00_NN.py
@@ -1,10 +1,11 @@
-input = [1, 2, 3]
-weights = [0.2, 0.8, -0.5]
-bias = 2
+input = [1.0, 2.0, 3.0,2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
 
 output= (input[0] * weights[0] +
           input[1] * weights[1] +
           input[2] * weights[2] +
+          input[3] * weights[3] +
           bias)
 
 print(output)

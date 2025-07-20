14fb0865ce0ddbfca84615d9fbd4905245455fbd
Santiago Cruz <byte.alpha.tech@gmail.com>
2025-07-19 21:49:33 -0400

    Matrix_Product

--- Neural_Networks/00_NN.py
+++ Neural_Networks/00_NN.py
@@ -28,5 +28,11 @@ output= [
         input[3] * weights3[3] + bias3]
           
           
-          
 print(output)
+
+
+a = [1, 2, 3]
+b = [2, 3, 4]
+
+dot_product = a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+print(dot_product)

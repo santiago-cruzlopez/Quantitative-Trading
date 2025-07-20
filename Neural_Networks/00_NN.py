2560fc9c85d70d5285b086a10ea0ebd1b5f950ba
Santiago Cruz <byte.alpha.tech@gmail.com>
2025-07-20 12:07:01 -0400

    One_Neuron_Numpy

--- Neural_Networks/00_NN.py
+++ Neural_Networks/00_NN.py
@@ -1,4 +1,6 @@
-input = [1.0, 2.0, 3.0,2.5]
+import numpy as np
+
+inputs = [1.0, 2.0, 3.0,2.5]
 
 weights1 = [0.2, 0.8, -0.5, 1.0]
 weights2 = [0.5, -0.91, 0.26, -0.5]
@@ -8,28 +10,29 @@ bias1 = 2.0
 bias2 = 3.0
 bias3 = 0.5
 
-output= [
+output1= [
         # Neuron 1:
-        input[0] * weights1[0] +
-        input[1] * weights1[1] +
-        input[2] * weights1[2] +
-        input[3] * weights1[3] + bias1,
+        inputs[0] * weights1[0] +
+        inputs[1] * weights1[1] +
+        inputs[2] * weights1[2] +
+        inputs[3] * weights1[3] + bias1,
 
         # Neuron 2:
-        input[0] * weights2[0] +
-        input[1] * weights2[1] +
-        input[2] * weights2[2] +
-        input[3] * weights2[3] + bias2,
+        inputs[0] * weights2[0] +
+        inputs[1] * weights2[1] +
+        inputs[2] * weights2[2] +
+        inputs[3] * weights2[3] + bias2,
 
         # Neuron 3:
-        input[0] * weights3[0] +
-        input[1] * weights3[1] +
-        input[2] * weights3[2] +
-        input[3] * weights3[3] + bias3]
+        inputs[0] * weights3[0] +
+        inputs[1] * weights3[1] +
+        inputs[2] * weights3[2] +
+        inputs[3] * weights3[3] + bias3]
           
-          
-print(output)
+output2 = np.dot(weights1, input) + bias1
 
+print(output1)
+print(output2)
 
 a = [1, 2, 3]
 b = [2, 3, 4]
